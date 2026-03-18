90c3c56d008eba69cdec7318cf10dfc6e9cbb591
Pradipghondge <pradipghondge@gmail.com>
2026-03-18 09:51:55 +0530

    change ui

--- pages/register.jsx
+++ pages/register.jsx
@@ -32,49 +32,48 @@ function register() {
 
     }
     return (
-        <div className=' overflow-y-hidden'>
-            <div className='flex'>
-            <img src='/back.png' />
-            <Link href='/admin'>
-            <h1 className='font-bold  text-white text-xl'> Back to Admin Panel</h1>
-            </Link>
-            </div>
-            <div className="w-full max-w-xs -mt-6 ml-auto mr-auto items-center ">
-                <form className="bg-white shadow-md rounded px-8 pt-2 pb-2 mb-4" onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            Username
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name='userName' value={userName} onChange={handleChangeInput} type="text" placeholder="Username" />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name='password' value={password} onChange={handleChangeInput} placeholder="******************" />
+        <div className='min-h-screen flex items-center justify-center px-4'>
+            <div className='w-full max-w-sm'>
+                <div className='flex items-center justify-center mb-4'>
+                    <img src='/back.png' />
+                    <Link href='/admin'>
+                        <h1 className='font-bold text-white text-xl'> Back to Admin Panel</h1>
+                    </Link>
+                </div>
+                <div className="w-full">
+                    <form className="bg-white shadow-md rounded px-8 pt-2 pb-2 mb-4" onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name='userName' value={userName} onChange={handleChangeInput} type="text" placeholder="Username" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name='password' value={password} onChange={handleChangeInput} placeholder="******************" />
 
-                    </div>
-                    <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
-              Role
-            </label>
-            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="role" type="text" name='role' value={role} onChange={handleChangeInput} placeholder="Enter Role" />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
+                                Role
+                            </label>
+                            <input className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="role" type="text" name='role' value={role} onChange={handleChangeInput} placeholder="Enter Role" />
 
-          </div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                            Register
-                        </button>
-                    </div>
-                </form>
-                <Modal isOpen={showModal} onClose={() => setShowModal(false)}  />
-               
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                    <Modal isOpen={showModal} onClose={() => setShowModal(false)}  />
+
+                </div>
             </div>
         </div>
     )
 }
 
 export default register
-
-
-
